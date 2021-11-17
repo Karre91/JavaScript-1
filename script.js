3a7de04bcb2627498dd926452c0240708dafc839
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2021-11-17 14:13:08 +0100

    Functions in JS,  buttons in html, guessNumber cancel button

--- script.js
+++ script.js
@@ -1,131 +1,77 @@
-/*
-//Del 1 
-for (let i = 1; i <= 10; i++)
-{
-    let product = i * i;
-    console.log(i +  " * " + i + " = " + product);
-}
+// DEL 1
 
-//Del 2
-for (let i = 1; i <= 10; i++)
-{
-    for (let j = 1; j <= 10; j++)
+// 1 
+function squareToTen (){
+    for (let i = 1; i <= 10; i++)
     {
-        let prod = i * j;
-        console.log(i +  " * " + j + " = " + prod);
+        let product = i * i;
+        console.log(i +  " * " + i + " = " + product);
     }
 }
 
-//Del 3
-let answer = Math.floor(Math.random() * 100 + 1);
-var guess = NaN;
-while (guess != answer)
-{    
-    guess = prompt("Gissa på ett tal: ");
-    if (guess < answer)
-    {
-        alert("För lågt!");
-    }
-    else if (guess > answer)
-    {
-        alert("För högt!");
-    }
-}
-if (guess == answer)
-{
-    alert("GRATTIS, du gissade rätt!");
-}
-*/
 
-//Del 4
+// 2
+function multiplication(){
+    for (let i = 1; i <= 10; i++)
+    {
+        for (let j = 1; j <= 10; j++)
+        {
+            let prod = i * j;
+            console.log(i +  " * " + j + " = " + prod);
+        }
+    }
+}
+
+
+// 3
+function guessNumber(){
+    let answer = Math.floor(Math.random() * 100 + 1);
+    var guess = NaN;
+    let exit = false;
+
+    while (exit == false){
+        guess = prompt("Gissa på ett tal:", "Skriv ett tal här");
+
+        if (guess){
+            if (guess < answer)
+                {
+                    alert("För lågt!");
+                }
+                else if (guess > answer)
+                {
+                    alert("För högt!");
+                }
+        } 
+        else {
+            exit = true;
+        }
+    }
+}
+
+
+// 4
+function robbersLanguage(){
 let inputSentence = "";
-let rovaspraket = "";
-let lettersOfSentence;
+let rovaspraket = " ";
 
-lettersOfSentence = prompt("Skriv in meningen du vill översätta:");
-//lettersOfSentence = inputSentence.split("");
-
-console.log("Användaren skrev in: " + inputSentence);
-
-console.log("Det innehåller såhär många char: " + lettersOfSentence.length);
+inputSentence = prompt("Skriv in meningen du vill översätta:");
 
 let vocal = ["a","e","y","u","i","o","å","ö","ä"];
 
-for (let i = 0; i < lettersOfSentence.length; i++){ //Går igenom varje plats i meningen
+for (let i = 0; i < inputSentence.length; i++){ //Går igenom varje plats i meningen
     
-    let result = vocal.indexOf(lettersOfSentence[i]); //Hittar index av vokalen
+    let result = vocal.indexOf(inputSentence[i]); //Hittar index av vokalen
     if (result == -1){ //Om vi inte hittar någon vokal
-        if (lettersOfSentence[i] == " "){ //Kolla om det är ett mellanslag
-            rovaspraket += lettersOfSentence[i]; //Lägg till mellanslag
+        if (inputSentence[i] == " "){ //Kolla om det är ett mellanslag
+            rovaspraket += inputSentence[i]; //Lägg till mellanslag
         }
         else{ //Det är inte vokal eller mellanslag
-            rovaspraket += lettersOfSentence[i] + "o" + lettersOfSentence[i]; //Spara konsonat + o + konsonant
+            rovaspraket += inputSentence[i] + "o" + inputSentence[i]; //Spara konsonat + o + konsonant
         }
     }
     else{ //Det var en vokal
-        rovaspraket += lettersOfSentence[i]; //Sparar vokalen
+        rovaspraket += inputSentence[i]; //Sparar vokalen
     }
 }
 console.log("Din mening i rövarspråker blir: " + rovaspraket);
-
-
-
-/*
-for (let i = 0; i < lettersOfSentence.length; i++){
-    //Om det INTE är en vokal    
-    if (lettersOfSentence[i] != "e"){
-        if (lettersOfSentence[i] != "y"){
-            if (lettersOfSentence[i] != "u"){
-                if (lettersOfSentence[i] != "i"){
-                    if (lettersOfSentence[i] != "o"){
-                        if (lettersOfSentence[i] != "å"){
-                            if (lettersOfSentence[i] != "a"){
-                                if (lettersOfSentence[i] != "ö"){
-                                    if (lettersOfSentence[i] != "ä"){
-                                        if (lettersOfSentence[i] != " "){
-                                            rovaspraket += lettersOfSentence[i];  
-                                            for (let j = i + 1; j < i + 2 ; j++)                               
-                                            rovaspraket += "o";                                                      
-                                            rovaspraket += lettersOfSentence[i];      
-                                        } //om det ÄR en vokal eller mellanslag
-                                        else{
-                                            rovaspraket += lettersOfSentence[i];
-                                        }
-                                    }                                        
-                                    else{
-                                        rovaspraket += lettersOfSentence[i];    
-                                    }
-                                }
-                                else{
-                                    rovaspraket += lettersOfSentence[i];    
-                                }
-                            }
-                            else{
-                                rovaspraket += lettersOfSentence[i]; 
-                            }
-                        }
-                        else{
-                            rovaspraket += lettersOfSentence[i];
-                        }
-                    }
-                    else{
-                        rovaspraket += lettersOfSentence[i];
-                    }
-                }
-                else{
-                    rovaspraket += lettersOfSentence[i];
-                }
-            }
-            else{
-                rovaspraket += lettersOfSentence[i];
-            }
-        }    
-        else{
-                rovaspraket += lettersOfSentence[i];
-        }
-    }
-    else{
-        rovaspraket += lettersOfSentence[i];    
-    }
-} 
-//console.log("Din mening i rövarspråker blir: " + rovaspraket);*/
+}
