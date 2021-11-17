db4d5ca00d3fd725c59959007facd3bff24e462e
Karin Eurenius <93973477+Karre91@users.noreply.github.com>
2021-11-17 17:55:49 +0100

    Created index, renamed files, started next weeks tasks

--- Del 2/script_Inl_1_Del_2.js
+++ Del 2/script_Inl_1_Del_2.js
@@ -1,1 +1,12 @@
 // 7
+inputfield = document.getElementById("leaveDate");
+          var date = new Date();
+         
+          datestring =
+            date.getFullYear() +
+            "-" +
+            (date.getMonth() + 1) +
+            "-" +
+            date.getDate() +
+            "T" +
+          inputfield.setAttribute("min", datestring);
